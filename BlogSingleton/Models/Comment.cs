ecb59278ce9358362a56a36c70eb0f9cef6afabc
taahachaudhry <t44h413@gmail.com>
2014-08-23 16:23:52 -0500

    Comment class

---
+++ BlogSingleton/Models/Comment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogSingleton.Models
+{
+    public class Comment
+    {
+        public string Message { get; set; }
+        public static int NextCommentID = 0;
+        public int CommentID { get; set; }
+        public int BlogPostID { get; set; }
+        public DateTime Date {get; set;}
+        public Comment(string message, int blogpostid)
+        {
+            Message = message;
+            CommentID = NextCommentID++;
+            BlogPostID = blogpostid;
+            Date = DateTime.Today;
+        }
+        public Comment()
+        {
+
+        }
+    }
+}
