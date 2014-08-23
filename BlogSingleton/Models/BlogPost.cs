fc5ca3ee33644801dc0ee8bf01872884c74cc69c
taahachaudhry <t44h413@gmail.com>
2014-08-23 16:19:58 -0500

    BlogPost class

---
+++ BlogSingleton/Models/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogSingleton.Models
+{
+    public class BlogPost
+    {
+        public static int NextID = 0;
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Post { get; set; }
+        public DateTime Date { get; set; }
+        public BlogPost(string title, string post)
+        {
+            ID = NextID++;
+            Title = title;
+            Post = post;
+            Date = DateTime.Today;
+        }
+        public BlogPost()
+        {
+
+        }
+    }
+}
